7f85f6af82213298089969054925bff0f76e32ab
David Zhao <davidzhao058@gmail.com>
2015-08-08 11:48:09 -0700

    Text to screen adding

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 $(document).keydown(function(ev){
     if(ev.keyCode == 13){
-        
+        $('#screen').html($('#screen').html() + '<br>' + $('#input').html());
+        $('#input').html('');
     }
 });
 
