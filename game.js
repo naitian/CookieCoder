960f007b8695ede0492233227623e34716dc8e5a
Naitian Zhou <nzhou2011@gmail.com>
2015-08-09 11:08:51 -0400

    Working on store

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-var javaTemplate = 'Cookie ~=new Cookie();';
+var javaCookieTemplate = 'Cookie ~=new Cookie();';
+var javaStoreTemplate = 'store.~(~);'
 
 $(document).keydown(function(ev){
     if(ev.keyCode == 13){
@@ -6,7 +7,7 @@ $(document).keydown(function(ev){
 			var code = $('#input').html();
 			code = code.replace(' =','=').replace('= ','=');
 			//check if varName is possible
-			var varName = code.substring(javaTemplate.indexOf('~'), code.indexOf(javaTemplate[javaTemplate.indexOf('~') + 1]));
+			var varName = code.substring(javaCookieTemplate.indexOf('~'), code.indexOf(javaCookieTemplate[javaCookieTemplate.indexOf('~') + 1]));
 			if(!varName){
 				$('#screen').html($('#screen').html() + '<br>$ ' + $('#input').html() + '<br>$ ' + 'error: cannot find symbol');
 				$('#input').html('');
@@ -17,7 +18,7 @@ $(document).keydown(function(ev){
 				$('#input').html('');
 				return;
 			}
-			var correctCode = javaTemplate.replace('~', varName);
+			var correctCode = javaCookieTemplate.replace('~', varName);
 			if(correctCode != code){
 				$('#screen').html($('#screen').html() + '<br>$ ' + $('#input').html() + '<br>$ ' + 'error: \';\' expected');
 				$('#input').html('');
@@ -28,6 +29,24 @@ $(document).keydown(function(ev){
         }
     }
 });
+
+function checkStore(){
+	var code = $('#input').html();
+	var func = code.substring(javaCookieTemplate.indexOf('~'), code.indexOf(javaCookieTemplate[javaCookieTemplate.indexOf('~') + 1]));
+	if(!func){
+		$('#screen').html($('#screen').html() + '<br>$ ' + $('#input').html() + '<br>$ ' + 'error: cannot find symbol');
+		$('#input').html('');
+		return;
+	}
+	if(!func[0].match(/[A-Za-z$_]/)){
+		$('#screen').html($('#screen').html() + '<br>$ ' + $('#input').html() + '<br>$ ' + 'error: not a statement');
+		$('#input').html('');
+		return;
+	}
+
+	
+
+}
 
 var terminalFocus = function(){
     $('#input').focus();
